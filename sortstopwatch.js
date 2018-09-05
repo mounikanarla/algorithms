99d52474dc6a549ba663ca71e7edd675c3187da8
mounikanarla <42598899+mounikanarla@users.noreply.github.com>
2018-09-05 19:11:19 +0530

    Add files via upload

--- sortstopwatch.js
+++ sortstopwatch.js
@@ -1,11 +1,24 @@
+ /**********************************************************************************************
+ *  Execution       :   1. default node         cmd> node anagram.js       
+ *  @purpose        : calculating elapsed time for seaching and sorting programs
+ *  @author         : mounika<nmounika358@gmail.com>
+ *  @version        : 10.9.0
+ *  @since          : 1-09-2018
+ *
+ ***********************************************************************************************/
+ /*returns an instance of prompt to read input from users*/
 var prompt=require('prompt-sync')();
+/* Including the folder address for calling the functions to main*/
 var utility=require('../utility/utility.js')
+
 function sortstopwatch(){
 var size=prompt("enter size value: ");
-
+/* calling getcurrenttime function to store start and stop values and stored in variable*/
 start=utility.getcurrenttime();
+/*calling the binary function in utility*/
 utility.binary(size);
 stop=utility.getcurrenttime();
+/*elapsed time is used to print the difference of start and stop time*/
 elapsetime1=utility.elapsedtime(start,stop);
 console.log("elapsed time: "+elapsetime1+"milliseconds");
 
@@ -38,7 +51,7 @@ utility.insertionword(size);
 stop=utility.getcurrenttime();
 elapsetime6=utility.elapsedtime(start,stop);
 console.log("elapsed time : "+elapsetime6+" milliseconds");
-
+/* storing all the elapsed time values in an array and returning in descending order*/
 var array=[elapsetime1,elapsetime2,elapsetime3,elapsetime4,elapsetime5,elapsetime6]
 array=array.sort(function(a,b){
     return b-a;
